352b0235a715f118ab25385d2a59fea12a794aaa
PranTanTheMan <Seriousbro23@gmail.com>
2022-02-24 23:11:09 -0600

    Image

--- App.js
+++ App.js
@@ -5,7 +5,14 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Image source={require("./assets/icon.png")} />
+      <Image
+        blurRadius={5}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
     </SafeAreaView>
   );
 }
